8d162a52351a761ce8cf219b7bfa958f0fdb0259
Jevonne Laing <jevonne63@gmail.com>
2022-11-11 22:23:27 -0500

    Initial commit

---
+++ src/main/java/com/example/sprintjabackend/SprintjaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintjabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintjaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintjaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprintjabackend/SprintjaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintjabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintjaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
