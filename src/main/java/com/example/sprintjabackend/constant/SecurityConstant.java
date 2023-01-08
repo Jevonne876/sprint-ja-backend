5091e89531a15cbdb9bcf404eb1049f432bf498a
jevonne <jevonne63@gmail.com.com>
2023-01-08 03:58:54 -0500

    SecurityConstant

--- src/main/java/com/example/sprintjabackend/constant/SecurityConstant.java
+++ src/main/java/com/example/sprintjabackend/constant/SecurityConstant.java
@@ -16,7 +16,20 @@ public class SecurityConstant {
     public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
     public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
     public static final String[] ADMIN_URLS = {"/admin/login", "/admin/sign-up", "/admin/dashboard"};
-    public static final String[] PUBLIC_URLS = {"/api/v1/register-new-user", "/api/v1/user-login","/api/v1/reset-password/**","/api/v1/send-query/**"};
+    public static final String[] PUBLIC_URLS = {"/api/v1/register-new-user",
+            "/api/v1/user-login","/api/v1/reset-password/**",
+            "/api/v1/send-query/**",
+            "/api/v1/admin/admin-login",
+            "/api/v1/admin/register-new-admin",
+            "/api/v1/admin/register-new-user",
+            "/api/v1/admin/get-application-data",
+            "/api/v1/admin/get-all-users",
+            "/api/v1/admin/get-all-user-packages",
+            "/api/v1/admin/get-all-user-packages-not-shipped",
+            "/api/v1/admin/get-all-user-packages-shipped",
+            "/api/v1/admin/get-all-user-packages-ready",
+            "/api/v1/admin/get-all-user-packages-delivered"
+            };
 
 
 }
